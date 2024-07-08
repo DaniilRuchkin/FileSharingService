be1ef0fa4e964215b43598553ec9daeb0c078a76
данил ручкин <danil-ruchkin01@mail.ru>
2024-07-08 15:18:35 +0300

    CorrectTheFormatting

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -3,10 +3,9 @@
 namespace FileSharingService.Repository;
 
 public interface IRepository
-    {
-        public Task SaveFileAsync(EntityFile entity);
-        public Task<EntityFile> GetFileAsync(string fileName);
-        public Task DeleteFileAsync(EntityFile entityFile);
-        public Task DeleteFileTimeAsync(DateTime deleteTime);
+{
+    public Task SaveFileAsync(EntityFile entity);
+    public Task<EntityFile> GetFileAsync(string fileName);
+    public Task DeleteFileAsync(EntityFile entityFile);
+    public Task DeleteFileTimeAsync(DateTime deleteTime);
 }
-
